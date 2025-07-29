7cd5da2b453241c97422acec356564ce059371a5
NikDemik <nikf24@mail.ru>
2025-07-29 08:23:04 +0300

    add prod db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,34 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@/generated/prisma';
+
 const prisma = new PrismaClient();
 
 async function main() {
-    const category = await prisma.product.create({
-        data: {
-            name: 'Шинопровод 60А, 4 полюса',
-            slug: 'busbar-60a-4p',
-            description: 'Базовый комплект троллейного шинопровода для промышленных нужд.',
-            imageUrl: '/images/busbar-60a.jpg',
-            categoryId: 1,
-            specs: {
-                poles: 4,
-                amperage: 60,
-                length: '10 м',
-            },
-        },
-    });
+	const category = await prisma.category.create({
+		data: {
+			name: 'Шинопровод 60А, 4 полюса',
+			slug: 'busbar-60a-4p',
+			products: {
+				create: {
+					name: 'Базовый комплект 60А',
+					slug: 'base-60a-kit',
+					description: 'Базовый комплект троллейного шинопровода для промышленных нужд.',
+					imageUrl: '/images/busbar-60a.jpg',
+					specs: {
+						poles: 4,
+						amperage: 60,
+						length: '10 м',
+					},
+					drawingUrl: '/drawings/60a.dxf',
+				},
+			},
+		},
+		include: { products: true },
+	});
 
-    console.log('Seed complete:', category);
+	console.log('Категория и продукт созданы:', category);
 }
 
 main()
-    .catch(console.error)
-    .finally(() => prisma.$disconnect());
+	.catch(console.error)
+	.finally(() => prisma.$disconnect());
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,15 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@/generated/prisma';
 
 const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined;
+	prisma: PrismaClient | undefined;
 };
 
 export const prisma =
-    globalForPrisma.prisma ??
-    new PrismaClient({
-        log: ['query'],
-    });
+	globalForPrisma.prisma ??
+	new PrismaClient({
+		log: ['query'],
+	});
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
